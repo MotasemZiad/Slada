0e99d4569d988d892d11351254bf3770b3574edc
MotasemZiad <miziad2000@gmail.com>
2022-06-30 13:48:15 +0300

    update auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:slada/screens/welcome_screen.dart';
 import 'package:slada/utils/color_manager.dart';
 import 'package:slada/utils/font_manager.dart';
 import 'package:slada/widgets/custom_button.dart';
@@ -34,115 +35,125 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            PositionedDirectional(
-              end: -150,
-              top: 80,
-              child: SvgPicture.asset(
-                'assets/svgs/bubble 01.svg',
+      body: SafeArea(
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              PositionedDirectional(
+                top: -130,
+                start: -100,
+                child: SvgPicture.asset('assets/svgs/bubble 04.svg'),
               ),
-            ),
-            PositionedDirectional(
-              top: -200,
-              start: -120,
-              child: SvgPicture.asset('assets/svgs/bubble 02.svg'),
-            ),
-            PositionedDirectional(
-              bottom: -200,
-              end: -120,
-              child: SvgPicture.asset('assets/svgs/bubble 02.svg'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 12,
+              PositionedDirectional(
+                top: -150,
+                start: -120,
+                child: SvgPicture.asset('assets/svgs/bubble 03.svg'),
               ),
-              child: SingleChildScrollView(
-                child: Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 250,
-                          bottom: 12,
-                        ),
-                        child: Text(
-                          'Login',
-                          style: FontManager.raleway.copyWith(
-                            color: ColorManager.black2Color,
-                            fontSize: 52,
-                            fontWeight: FontWeight.bold,
+              PositionedDirectional(
+                end: -150,
+                top: 80,
+                child: SvgPicture.asset('assets/svgs/bubble 01.svg'),
+              ),
+              PositionedDirectional(
+                bottom: -200,
+                end: -120,
+                child: SvgPicture.asset('assets/svgs/bubble 02.svg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                child: SingleChildScrollView(
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 300,
+                            bottom: 12,
                           ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Good to see you back!",
-                            style: FontManager.nunitoSans.copyWith(
-                              fontSize: 20,
+                          child: Text(
+                            'Login',
+                            style: FontManager.raleway.copyWith(
                               color: ColorManager.black2Color,
-                              fontWeight: FontWeight.w300,
+                              fontSize: 52,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          const Icon(
-                            Icons.favorite,
-                            color: ColorManager.blackColor,
-                            size: 16.0,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      CustomTextField(
-                        label: 'EMAIL',
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        icon: 'assets/svgs/at.svg',
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      CustomTextField(
-                        label: 'PASSWORD',
-                        controller: _passwordController,
-                        isPassword: true,
-                        icon: 'assets/svgs/Icon awesome-lock.svg',
-                      ),
-                      const SizedBox(
-                        height: 32.0,
-                      ),
-                      CustomButton(
-                        label: 'Login',
-                        onTap: () {},
-                      ),
-                      const SizedBox(
-                        height: 12.0,
-                      ),
-                      Center(
-                        child: CustomTextButton(
-                          label: 'Cancel',
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
                         ),
-                      ),
-                    ],
+                        Row(
+                          children: [
+                            Text(
+                              "Good to see you back!",
+                              style: FontManager.nunitoSans.copyWith(
+                                fontSize: 20,
+                                color: ColorManager.black2Color,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            const Icon(
+                              Icons.favorite,
+                              color: ColorManager.blackColor,
+                              size: 16.0,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        CustomTextField(
+                          label: 'EMAIL',
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          icon: 'assets/svgs/at.svg',
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        CustomTextField(
+                          label: 'PASSWORD',
+                          controller: _passwordController,
+                          isPassword: true,
+                          icon: 'assets/svgs/Icon awesome-lock.svg',
+                        ),
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        CustomButton(
+                          label: 'Login',
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const WelcomeScreen(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 12.0,
+                        ),
+                        Center(
+                          child: CustomTextButton(
+                            label: 'Cancel',
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -38,98 +38,100 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            PositionedDirectional(
-              end: -150,
-              top: 80,
-              child: SvgPicture.asset('assets/svgs/bubble 01.svg'),
-            ),
-            PositionedDirectional(
-              top: -200,
-              start: -120,
-              child: SvgPicture.asset('assets/svgs/bubble 02.svg'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 12,
+      body: SafeArea(
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              PositionedDirectional(
+                end: -150,
+                top: 80,
+                child: SvgPicture.asset('assets/svgs/bubble 01.svg'),
               ),
-              child: SingleChildScrollView(
-                child: Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 120,
-                          bottom: 48,
-                        ),
-                        child: Text(
-                          'Create\nAccount',
-                          style: FontManager.raleway.copyWith(
-                            color: ColorManager.black2Color,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
+              PositionedDirectional(
+                top: -200,
+                start: -120,
+                child: SvgPicture.asset('assets/svgs/bubble 02.svg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                child: SingleChildScrollView(
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 120,
+                            bottom: 48,
+                          ),
+                          child: Text(
+                            'Create\nAccount',
+                            style: FontManager.raleway.copyWith(
+                              color: ColorManager.black2Color,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      CustomImagePicker(onTap: () {}),
-                      const SizedBox(
-                        height: 48.0,
-                      ),
-                      CustomTextField(
-                        label: 'NAME',
-                        controller: _nameController,
-                        icon: 'assets/svgs/user.svg',
-                        keyboardType: TextInputType.name,
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      CustomTextField(
-                        label: 'EMAIL',
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        icon: 'assets/svgs/at.svg',
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      CustomTextField(
-                        label: 'PASSWORD',
-                        controller: _passwordController,
-                        isPassword: true,
-                        icon: 'assets/svgs/Icon awesome-lock.svg',
-                      ),
-                      const SizedBox(
-                        height: 32.0,
-                      ),
-                      CustomButton(
-                        label: 'Register',
-                        onTap: () {},
-                      ),
-                      const SizedBox(
-                        height: 12.0,
-                      ),
-                      Center(
-                        child: CustomTextButton(
-                          label: 'Cancel',
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
+                        CustomImagePicker(onTap: () {}),
+                        const SizedBox(
+                          height: 48.0,
                         ),
-                      ),
-                    ],
+                        CustomTextField(
+                          label: 'NAME',
+                          controller: _nameController,
+                          icon: 'assets/svgs/user.svg',
+                          keyboardType: TextInputType.name,
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        CustomTextField(
+                          label: 'EMAIL',
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          icon: 'assets/svgs/at.svg',
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        CustomTextField(
+                          label: 'PASSWORD',
+                          controller: _passwordController,
+                          isPassword: true,
+                          icon: 'assets/svgs/Icon awesome-lock.svg',
+                        ),
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        CustomButton(
+                          label: 'Register',
+                          onTap: () {},
+                        ),
+                        const SizedBox(
+                          height: 12.0,
+                        ),
+                        Center(
+                          child: CustomTextButton(
+                            label: 'Cancel',
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
